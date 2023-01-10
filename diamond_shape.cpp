022328307bba523e2d64428717e8b7d546a9f61e
Muhammad Ali <89317342+SeAliijaz@users.noreply.github.com>
2023-01-11 00:41:49 +0500

    Create diamond_shape.cpp

---
+++ diamond_shape.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter the number of rows: ";
+    cin >> n;
+
+    // Print upper half of the diamond
+    for (int i = 1; i <= n; i++)
+    {
+        // Print spaces
+        for (int j = 1; j <= n - i; j++)
+            cout << " ";
+
+        // Print asterisks
+        for (int j = 1; j <= 2 * i - 1; j++)
+            cout << "*";
+
+        cout << endl;
+    }
+
+    // Print lower half of the diamond
+    for (int i = n - 1; i >= 1; i--)
+    {
+        // Print spaces
+        for (int j = 1; j <= n - i; j++)
+            cout << " ";
+
+        // Print asterisks
+        for (int j = 1; j <= 2 * i - 1; j++)
+            cout << "*";
+
+        cout << endl;
+    }
+
+    return 0;
+}
